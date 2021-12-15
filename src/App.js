d17c35291f610fe58b2a3b3c9301e206de72e43f
Sayd-Ali <usmanov.guno@mail.ru>
2021-12-15 23:17:08 +0300

    Добавил первую версию авторизации

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import HomePage from './components/Pages/HomePage';
+import HomePage from './components/Pages/HomePage/HomePage';
 import { Route, Routes } from 'react-router-dom';
 import FindInterlocutorPage from './components/Pages/FindInterlocutorPage';
 import FindTeacherPage from './components/Pages/FindTeacherPage';
 import MessagePage from './components/Pages/MessagePage';
+import SignUpPage from './components/Pages/SignUpPage/SignUpPage';
+import SignInPage from './components/Pages/SignInPage/SignInPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage/>} />
+			<Route path='/signUp' element={<SignUpPage/>} />
+			<Route path='/signIn' element={<SignInPage/>} />
       <Route path='/interlocutor' element={<FindInterlocutorPage/>} />
       <Route path='/teacher' element={<FindTeacherPage/>} />
       <Route path='/message' element={<MessagePage/>} />
