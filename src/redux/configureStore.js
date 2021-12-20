7206aeebb3ae95da0ca93f80098c50d766e7f65b
khamzat <khamz@mail.ru>
2021-12-20 22:13:09 +0300

    начальная версия карточек

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { authReducer } from "./features/Auth";
 import { logger } from 'redux-logger/src';
 import { userReducer } from './features/User';
+import { profileReducer } from './features/ProfileReducer';
 
 export const store = createStore(
   combineReducers({
